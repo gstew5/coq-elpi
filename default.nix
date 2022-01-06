0e2bfba4cc92a3ea09b4b35ada40a38c0c856873
Benjamin Gregoire <Benjamin.Gregoire@inria.fr>
2022-01-06 13:43:41 +0100

    default.nix

--- default.nix
+++ default.nix
@@ -1,45 +1,12 @@
-{ pkgs ? import <nixpkgs> {}
-, inCI ? false
-, coqDeps ? !inCI
-, ocamlDeps ? !inCI
-, testDeps ? !inCI
-, devTools ? !inCI
-, enableFramePointers ? false
-}:
-
-with pkgs;
-
-let inherit (lib) optionals; in
-
-let coqPackages = coqPackages_8_14; in
-
-let inherit (coqPackages.coq) ocamlPackages; in
-
-let oP =
-  if enableFramePointers
-  then ocamlPackages.overrideScope' (self: super: {
-    ocaml = super.ocaml.overrideAttrs (o: {
-      configureFlags = o.configureFlags ++ [ "--enable-frame-pointers" ];
-    });
-  })
-  else ocamlPackages
-; in
-
-if !lib.versionAtLeast oP.ocaml.version "4.08"
-then throw "Jasmin requires OCaml ≥ 4.08"
-else
-
-stdenv.mkDerivation {
-  name = "jasmin-0";
-  src = null;
-  buildInputs = []
-    ++ optionals coqDeps [ coqPackages.coq coqPackages.coq.ocamlPackages.ocaml ]
-    ++ optionals ocamlDeps [ocamlPackages.elpi]
-    ++ optionals testDeps ([ ocamlPackages.apron.out ] ++ (with python3Packages; [ python pyyaml ]))
-    ++ optionals ocamlDeps ([ mpfr ppl ] ++ (with oP; [
-         ocaml findlib ocamlbuild
-         (batteries.overrideAttrs (o: { doCheck = false; }))
-         menhir (oP.menhirLib or null) zarith camlidl apron yojson ]))
-    ++ optionals devTools (with oP; [ merlin ])
-    ;
-}
+{ config ? {}, withEmacs ? false, print-env ? false, do-nothing ? false,
+  update-nixpkgs ? false, ci-matrix ? false,
+  override ? {}, ocaml-override ? {}, global-override ? {},
+  bundle ? null, job ? null, inNixShell ? null, src ? ./.,
+}@args:
+let auto = fetchGit {
+  url = "https://github.com/coq-community/coq-nix-toolbox.git";
+  ref = "master";
+  rev = import .nix/coq-nix-toolbox.nix;
+};
+in
+import auto ({inherit src;} // args)
